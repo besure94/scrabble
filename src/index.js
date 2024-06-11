56ebd13ea4841651313126183ee361b5c321adf8
Brian Scherner <brianscherner@gmail.com>
2024-06-11 15:54:50 -0700

    Create game board and event listener to start game.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+window.addEventListener("load", function() {
+  document.getElementById("start-game").addEventListener("click", function () {
+    document.getElementById("start-game").setAttribute("class", "hidden");
+    document.getElementById("game-div").removeAttribute("class", "hidden");
+    // need function that removes two tiles from tile-bag obj, compares both tiles to "A", and determines who plays first.
+    // need form so that user can submit what word they want to play.
+  });
+});
