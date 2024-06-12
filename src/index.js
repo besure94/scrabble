0fad66bb16bf25a227edca66ed2df8cfde1174bf
Brian Scherner <brianscherner@gmail.com>
2024-06-12 15:52:32 -0700

    Dynamically create game board, and place first letter of word on center tile for player's first turn.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,40 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
+function createGameBoard() {
+  const boxesPerRow = 15;
+  const boxesPerColumn = 15;
+  const totalBoxes = boxesPerRow * boxesPerColumn;
+  const gameBoard = document.getElementById("gameBoard");
+
+  for (let i = 0; i < totalBoxes; i++) {
+    let box = document.createElement("div");
+    box.setAttribute("class", "box");
+    gameBoard.appendChild(box);
+    if (i === 112) {
+      box.setAttribute("id", "center-star");
+      console.log(box);
+      box.innerHTML = "*";
+    }
+  }
+}
+
+function playFirstWordOfGame() {
+  const playedWord = document.getElementById("playedWord").value;
+  let playedWordArray = playedWord.split('');
+  let firstLetter = playedWordArray[0];
+  let centerStar = document.getElementById("center-star");
+  centerStar.innerHTML = firstLetter;
+}
+
 window.addEventListener("load", function() {
   document.getElementById("start-game").addEventListener("click", function () {
     document.getElementById("start-game").setAttribute("class", "hidden");
     document.getElementById("game-div").removeAttribute("class", "hidden");
-    // need function that removes two tiles from tile-bag obj, compares both tiles to "A", and determines who plays first.
-    // need form so that user can submit what word they want to play.
+    createGameBoard();
   });
-  document.getElementById("playButton").addEventListener("click", function () {
+  document.getElementById("playButton").addEventListener("click", function (event) {
     event.preventDefault();
-    const playedWord = document.getElementById("playedWord").value;
-    console.log(playedWord);
+    playFirstWordOfGame();
   });
 });
