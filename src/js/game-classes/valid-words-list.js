a0ff66a8c9f8c41df4ebbf70e11677c2209242ef
igl-myrick <igl.myrick@gmail.com>
2024-06-13 14:22:04 -0700

    add ValidWordsList class

---
+++ src/js/game-classes/valid-words-list.js
@@ -0,0 +1,5 @@
+export default class ValidWordsList {
+  constructor() {
+    this.wordList = ['be', 'to', 'of', 'do', 'we', 'the', 'and', 'not', 'say', 'for', 'gym', 'have', 'with', 'this', 'from', 'what', 'exit', 'just', 'quick', 'about', 'blaze', 'plant', 'count', 'zebra', 'number', 'people', 'before', 'desert', 'through', 'between'];
+  }
+}
