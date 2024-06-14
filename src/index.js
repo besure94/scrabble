3cd76d8f3c2709526ca3f5cdd19e9c6c127a0ae6
igl-myrick <igl.myrick@gmail.com>
2024-06-14 15:38:53 -0700

    work-in-progress commit for populating tiles

--- src/index.js
+++ src/index.js
@@ -12,12 +12,8 @@ function determineTurnOrder() {
     player2Roll = Math.floor(Math.random() * 27);
   }
   if (player1Roll > player2Roll) {
-    // player1.turn = "true";
-    // player2.turn = "false";
     return "player1";
   } else {
-    // player1.turn = "false";
-    // player2.turn = "true";
     return "player2";
   }
 }
@@ -39,46 +35,72 @@ function createGameBoard() {
   }
 }
 
-function createTilesArray() {
-  // variable containing tiles for testing purposes
-  const tilesArrayPlayer1 = ['a', 'p', 'r', 'i', 'c', 'o', 't'];
-  const tilesArrayPlayer2 = ['m', 'o', 'n', 'k', 'e', 'y', 's'];
-  console.log(tilesArrayPlayer1, tilesArrayPlayer2);
+function createTilesArray(playerOne, playerTwo) {
   const player1TilesDiv = document.getElementById("player-one-tiles");
   const player2TilesDiv = document.getElementById("player-two-tiles");
   const tilesPerPlayer = 7;
+  let playerOneCurrentTiles = playerOne.tiles;
+  let playerTwoCurrentTiles = playerTwo.tiles;
+  console.log(playerOneCurrentTiles);
+  console.log(playerTwoCurrentTiles);
 
   for (let i = 0; i < tilesPerPlayer; i++) {
     let tile = document.createElement("div");
     tile.setAttribute("class", "tile");
+    tile.innerHTML = playerOneCurrentTiles;
     player1TilesDiv.appendChild(tile);
-    // player2TilesDiv.appendChild(tile);
   }
+  console.log(player1TilesDiv);
 
+  for (let i = 0; i <= playerOneCurrentTiles; i++) {
+    document.getElementsByClassName("tile").innerHTML = playerOneCurrentTiles[i];
+  }
+  
   for (let i = 0; i < tilesPerPlayer; i++) {
     let tile = document.createElement("div");
     tile.setAttribute("class", "tile");
-    // player1TilesDiv.appendChild(tile);
     player2TilesDiv.appendChild(tile);
   }
-
-  console.log("Player 1 tiles: ", player1TilesDiv);
-  console.log("Player 2 tiles: ", player2TilesDiv);
+  console.log(player2TilesDiv);
 
 }
 
-function placeTilesOnBoard() {
-  determineTurnOrder();
+function placeTilesOnBoard(playerTurn) {
   const player1Div = document.getElementById("player-one");
   const player2Div = document.getElementById("player-two");
-  if (player1.turn === "true") {
+  if (playerTurn === "player1") {
     player2Div.setAttribute("class", "hidden");
-  } else if (player1.turn === "false") {
+  } else if (playerTurn === "player2") {
     player1Div.setAttribute("class", "hidden");
   }
 }
 
-placeTilesOnBoard();
+// function populateTiles(currentPlayer) {
+//   let currentTiles = currentPlayer.tiles;
+//   // console.log(currentDiv);
+//   // currentDiv.forEach(function(element, index) {
+//   //   element.append(currentTiles[index]);
+//   // });
+//   console.log(currentTiles);
+// }
+
+window.addEventListener("load", function() {
+  document.getElementById("start-game").addEventListener("click", function () {
+    document.getElementById("start-game").setAttribute("class", "hidden");
+    document.getElementById("game-div").removeAttribute("class", "hidden");
+    createGameBoard();
+    // createTilesArray();
+    let playerTurn = determineTurnOrder();
+    placeTilesOnBoard(playerTurn);
+    let tileBag = new TileBag();
+    const player1 = new Player(0, [], '');
+    const player2 = new Player(0, [], '');
+    player1.drawTiles(tileBag);
+    player2.drawTiles(tileBag);
+    // populateTiles(player1);
+    createTilesArray(player1, player2);
+  });
+});
 
 // function playFirstWordOfGame() {
 //   const playedWord = document.getElementById("playedWord").value;
@@ -88,24 +110,11 @@ placeTilesOnBoard();
 //   centerStar.innerHTML = firstLetter;
 // }
 
-window.addEventListener("load", function() {
-  document.getElementById("start-game").addEventListener("click", function () {
-    document.getElementById("start-game").setAttribute("class", "hidden");
-    document.getElementById("game-div").removeAttribute("class", "hidden");
-    createGameBoard();
-    determineTurnOrder();
-    let tileBag = new TileBag();
-    const player1 = new Player(0, [], true, '');
-    // const player2 = new Player();
-    player1.drawTiles(tileBag);
-    console.log(player1.tiles);
-    document.getElementById("playButton").addEventListener("click", function (event) {
-      event.preventDefault();
-      playFirstWordOfGame();
-      const playedWord = document.getElementById("playedWord").value;
-      // console.log(player1.score);
-      player1.scoreWord(playedWord);
-      console.log(player1.score);
-    });
-  });
-});
+// document.getElementById("playButton").addEventListener("click", function (event) {
+//   event.preventDefault();
+//   // playFirstWordOfGame();
+//   const playedWord = document.getElementById("playedWord").value;
+//   // console.log(player1.score);
+//   player1.scoreWord(playedWord);
+//   console.log(player1.score);
+// });
